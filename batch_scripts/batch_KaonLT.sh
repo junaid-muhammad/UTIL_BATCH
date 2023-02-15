219cf2646aa927ec2a778fd16431d3c0e831ce12
Richard Trotta <trotta@cua.edu>
2023-02-15 10:35:16 -0500

    Updated Q2=3p0 run number file names to be consistent with others. Added LTSep analysis to batch job submission script.

--- batch_scripts/batch_KaonLT.sh
+++ batch_scripts/batch_KaonLT.sh
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2022-06-18 14:57:17 trottar"
+# Time-stamp: "2023-02-14 18:16:45 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -17,48 +17,163 @@
 ### A batch submission script based on an earlier version by Richard Trotta, Catholic University of America                       
 ##### Modify required resources as needed!
 
+echo 
+echo
+echo
 echo "Running as ${USER}"
 
-RUNTYPE=$1
-TARGET=$2
-RunList=$3
-MAXEVENTS=$4
-if [[ -z "$1" || ! "$RUNTYPE" =~ Prod|Lumi|HeePSing|HeePCoin|fADC|Optics|HGCer ]]; then # Check the 2nd argument was provided and that it's one of the valid options
-    echo ""
-    echo "I need a valid run type"
-    while true; do
-	echo ""
-	read -p "Please type in a run type from - Prod - Lumi - HeePSing - HeePCoin - fADC - Optics - HGCer - Case sensitive! - or press ctrl-c to exit : " RUNTYPE
-	case $RUNTYPE in
-	    '');; # If blank, prompt again
-	    'Prod'|'Lumi'|'HeePSing'|'HeePCoin'|'Optics'|'fADC'|'HGCer') break;; # If a valid option, break the loop and continue
-	esac
-    done
-fi
-if [[ -z "$2" || ! "$TARGET" =~ LH2|LD2|Dummy10cm|Carbon0p5|AuFoil|Optics1|Optics2|CarbonHole ]]; then # Check the 3rd argument was provided and that it's one of the valid options
-    echo ""
-    echo "I need a valid target"
-    while true; do	
-	echo ""
-	read -p "Please type in a target from - LH2 - LD2 - Dummy10cm - Carbon0p5 - AuFoil - Optics1 - Optics2 - CarbonHole - Case sensitive! - or press ctrl-c to exit : " TARGET
-	case $TARGET in
-	    '');; # If blank, prompt again
-	    'LH2'|'LD2'|'Dummy10cm'|'Carbon0p5'|'AuFoil'|'Optics1'|'Optics2'|'CarbonHole') break;; # If a valid option, break the loop and continue
-	esac
-    done
-fi
-if [[ -z "$3" ]]; then
-    echo "I need a run list process!"
-    echo "Please provide a run list as input"
-    exit 2
-fi
-if [[ $4 -eq "" ]]; then
-    echo "Only Run Number entered...I'll assume -1 events!" 
-    MAXEVENTS=-1 
-fi
+# Flag definitions (flags: h, a)
+while getopts 'ha' flag; do
+    case "${flag}" in
+        h) 
+        echo "--------------------------------------------------------------"
+        echo "./batch_KaonLT.sh -{flags} {variable arguments, see help}"
+	echo
+        echo "Description: Runs scripts for batch submission"
+        echo "--------------------------------------------------------------"
+        echo
+        echo "The following flags can be called for the heep analysis..."
+	echo "    If no flags called arguments are..."
+	echo "        RUNTYPE=arg1, TARGET=arg2, RunList=arg3, MAXEVENTS=arg4"
+        echo "    -h, help"
+        echo "    -a, Runs LTSep analysis"
+	echo "        EPSILON=arg1, PHIVAL=arg2, Q2=arg3, W=arg4, target=arg5"
+	echo
+	echo " Avaliable Kinematics..."	
+	echo "                      Q2=5p5, W=3p02"
+	echo "                      Q2=4p4, W=2p74"
+	echo "                      Q2=3p0, W=3p14"
+	echo "                      Q2=3p0, W=2p32"
+	echo "                      Q2=2p1, W=2p95"
+	echo "                      Q2=0p5, W=2p40"
+        exit 0
+        ;;
+	a) a_flag='true' ;;
+        *) print_usage
+        exit 1 ;;
+    esac
+done
 
 UTILPATH="/group/c-kaonlt/USERS/${USER}/hallc_replay_lt/UTIL_BATCH"
-ANASCRIPT="'${UTILPATH}/Analysis_Scripts/KaonLT.sh' ${RUNTYPE} ${TARGET}"
+
+if [[ $a_flag = 'true' ]]; then
+    EPSILON=$(echo "$2" | tr '[:upper:]' '[:lower:]')
+    PHIVAL=$(echo "$3" | tr '[:upper:]' '[:lower:]')
+    Q2=$4
+    W=$5
+    TARGET=$(echo "$6" | tr '[:upper:]' '[:lower:]')
+    RunList=$7
+
+    if [[ -z "$2" || ! "$EPSILON" =~ high|low ]]; then # Check the 1st argument was provided and that it's one of the valid options
+	echo ""
+	echo "I need a valid epsilon..."
+	while true; do
+	    echo ""
+	    read -p "Press ctrl-c to exit : " EPSILON
+	    case $EPSILON in
+		'');; # If blank, prompt again
+		'high'|'low') break;; # If a valid option, break the loop and continue
+	    esac
+	done
+    fi
+    if [[ -z "$3" || ! "$PHIVAL" =~ right|left|center ]]; then # Check the 1st argument was provided and that it's one of the valid options
+	echo ""
+	echo "I need a valid phi value..."
+	while true; do
+	    echo ""
+	    read -p "Press ctrl-c to exit : " PHIVAL
+	    case $PHIVAL in
+		'');; # If blank, prompt again
+		'right'|'left'|'center') break;; # If a valid option, break the loop and continue
+	    esac
+	done
+    fi
+    if [[ -z "$4" || ! "$Q2" =~ 5p5|4p4|3p0|2p1|0p5 ]]; then # Check the 2nd argument was provided and that it's one of the valid options
+	echo ""
+	echo "I need a valid Q2..."
+	while true; do
+	    echo ""
+	    read -p "Q2 must be one of - [5p5 - 4p4 - 3p0 - 2p1 - 0p5] - or press ctrl-c to exit : " Q2
+	    case $Q2 in
+		'');; # If blank, prompt again
+		'5p5'|'4p4'|'3p0'|'2p1'|'0p5') break;; # If a valid option, break the loop and continue
+	    esac
+	done
+    fi
+    if [[ -z "$5" || ! "$W" =~ 3p02|2p74|3p14|2p32|2p95|2p40 ]]; then # Check the 3rd argument was provided and that it's one of the valid options
+	echo ""
+	echo "I need a valid W..."
+	while true; do
+	    echo ""
+	    read -p "W must be one of - [3p02 - 2p74 - 3p14 - 2p32 - 2p95 - 2p40] - or press ctrl-c to exit : " W
+	    case $W in
+		'');; # If blank, prompt again
+		'3p02'|'2p74'|'3p14'|'2p32'|'2p95'|'2p40') break;; # If a valid option, break the loop and continue
+	    esac
+	done
+    fi
+    if [[ -z "$6" || ! "$TARGET" =~ lh2|dummy ]]; then # Check the 3rd argument was provided and that it's one of the valid options
+	echo ""
+	echo "I need a valid target type..."
+	while true; do
+	    echo ""
+	    read -p "Target type must be one of - [lh2 - dummy] - or press ctrl-c to exit : " TARGET
+	    case $TARGET in
+		'');; # If blank, prompt again
+		'lh2'|'dummy') break;; # If a valid option, break the loop and continue
+	    esac
+	done
+    fi
+    if [[ -z "$7" ]]; then
+	echo "I need a run list process!"
+	echo "Please provide a run list as input"
+	exit 2
+    fi
+
+    ANASCRIPT="'${UTILPATH}/Analysis_Scripts/KaonLT.sh' -a ${EPSILON} ${PHIVAL} ${Q2} ${W} ${TARGET}"
+
+else
+    RUNTYPE=$1
+    TARGET=$2
+    RunList=$3
+    MAXEVENTS=$4
+    if [[ -z "$1" || ! "$RUNTYPE" =~ Prod|Lumi|HeePSing|HeePCoin|fADC|Optics|HGCer ]]; then # Check the 2nd argument was provided and that it's one of the valid options
+	echo ""
+	echo "I need a valid run type"
+	while true; do
+	    echo ""
+	    read -p "Please type in a run type from - Prod - Lumi - HeePSing - HeePCoin - fADC - Optics - HGCer - Case sensitive! - or press ctrl-c to exit : " RUNTYPE
+	    case $RUNTYPE in
+		'');; # If blank, prompt again
+		'Prod'|'Lumi'|'HeePSing'|'HeePCoin'|'Optics'|'fADC'|'HGCer') break;; # If a valid option, break the loop and continue
+	    esac
+	done
+    fi
+    if [[ -z "$2" || ! "$TARGET" =~ LH2|LD2|Dummy10cm|Carbon0p5|AuFoil|Optics1|Optics2|CarbonHole ]]; then # Check the 3rd argument was provided and that it's one of the valid options
+	echo ""
+	echo "I need a valid target"
+	while true; do	
+	    echo ""
+	    read -p "Please type in a target from - LH2 - LD2 - Dummy10cm - Carbon0p5 - AuFoil - Optics1 - Optics2 - CarbonHole - Case sensitive! - or press ctrl-c to exit : " TARGET
+	    case $TARGET in
+		'');; # If blank, prompt again
+		'LH2'|'LD2'|'Dummy10cm'|'Carbon0p5'|'AuFoil'|'Optics1'|'Optics2'|'CarbonHole') break;; # If a valid option, break the loop and continue
+	    esac
+	done
+    fi
+    if [[ -z "$3" ]]; then
+	echo "I need a run list process!"
+	echo "Please provide a run list as input"
+	exit 2
+    fi
+    if [[ $4 -eq "" ]]; then
+	echo "Only Run Number entered...I'll assume -1 events!" 
+	MAXEVENTS=-1 
+    fi
+
+    ANASCRIPT="'${UTILPATH}/Analysis_Scripts/KaonLT.sh' ${RUNTYPE} ${TARGET}"
+
+fi
 
 # 15/02/22 - SJDK - Added the swif2 workflow as a variable you can specify here
 Workflow="LTSep_${USER}" # Change this as desired
@@ -84,7 +199,11 @@ while true; do
 		    MSSstub='/mss/hallc/spring17/raw/coin_all_%05d.dat'
 		fi
 		##Output batch job file##
-		batch="${USER}_${runNum}_FullReplay_${RUNTYPE}_Job.txt"
+		if [[ $a_flag = 'true' ]]; then
+		    batch="${USER}_${runNum}_FullReplay_LTSep_Job.txt"
+		else
+		    batch="${USER}_${runNum}_FullReplay_${RUNTYPE}_Job.txt"
+		fi
                 tape_file=`printf $MSSstub $runNum`
 		TapeFileSize=$(($(sed -n '4 s/^[^=]*= *//p' < $tape_file)/1000000000))
 		if [[ $TapeFileSize == 0 ]];then
@@ -102,7 +221,11 @@ while true; do
                 echo "PROJECT: c-kaonlt" >> ${batch} # Or whatever your project is!
                 echo "TRACK: analysis" >> ${batch}
                 #echo "TRACK: debug" >> ${batch} ### Use for testing
-                echo "JOBNAME: KaonLT_${runNum}" >> ${batch}
+		if [[ $a_flag = 'true' ]]; then
+		    echo "JOBNAME: KaonLT_LTSep_${runNum}" >> ${batch}
+		else
+		    echo "JOBNAME: KaonLT_${RUNTYPE}_${runNum}" >> ${batch}
+		fi
                 # Request disk space depending upon raw file size
                 echo "DISK_SPACE: "$(( $TapeFileSize * 2 ))" GB" >> ${batch}
 		if [[ $TapeFileSize -le 45 ]]; then
@@ -113,8 +236,12 @@ while true; do
 		#echo "OS: centos7" >> ${batch}
                 echo "CPU: 1" >> ${batch} ### hcana single core, setting CPU higher will lower priority!
 		echo "INPUT_FILES: ${tape_file}" >> ${batch}
-		#echo "TIME: 1" >> ${batch} 
-		echo "COMMAND:${ANASCRIPT} ${runNum} ${MAXEVENTS}" >> ${batch}
+		#echo "TIME: 1" >> ${batch}
+		if [[ $a_flag = 'true' ]]; then
+		    echo "COMMAND:${ANASCRIPT} ${runNum}" >> ${batch}
+		else
+		    echo "COMMAND:${ANASCRIPT} ${runNum} ${MAXEVENTS}" >> ${batch}
+		fi
                 echo "Submitting batch"
  		eval "swif2 add-jsub ${Workflow} -script ${batch} 2>/dev/null" # Swif2 job submission, uses old jsub scripts
                 echo " "
